821bfac8632a66d338ac5f582bb0eeda45fe07c0
Tero Kujala <tero.s.kujala@gmail.com>
2013-03-27 17:40:01 +0200

    Kirjan lisays kontrolleriin, hiberate lisatty pomiin, index ohjaa kirjan sivulle

--- BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/controller/BookController.java
+++ BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/controller/BookController.java
@@ -1,14 +1,33 @@
 package ohtaajat.bibtex.viitehallinta.controller;
 
+import javax.validation.Valid;
+import ohtaajat.bibtex.viitehallinta.data.Book;
+import ohtaajat.bibtex.viitehallinta.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
     
-    @RequestMapping
-    public String list(){
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "book";
+        }
+        bookService.create(book);
+        return "redirect:book";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String list(@ModelAttribute("book") Book book) {
         return "book";
     }
 }
--- BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/service/JpaBookService.java
+++ BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/service/JpaBookService.java
@@ -5,6 +5,7 @@ import ohtaajat.bibtex.viitehallinta.data.Book;
 import ohtaajat.bibtex.viitehallinta.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class JpaBookService implements BookService {
@@ -13,21 +14,25 @@ public class JpaBookService implements BookService {
     private BookRepository brepo;
 
     @Override
+    @Transactional(readOnly = false)
     public void create(Book book) {
         brepo.save(book);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Book> list() {
         return brepo.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Book findById(Long id) {
         return brepo.findOne(id);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void delete(Long id) {
         brepo.delete(id);
     }
