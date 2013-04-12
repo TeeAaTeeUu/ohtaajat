de198fc2ecd82fb4fdfaa928c7b9a9f0b4eafce5
Tero Kujala <tero.s.kujala@gmail.com>
2013-04-12 11:06:43 +0300

    Validointi pois

--- BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/validators/BookValidator.java
+++ BibTex-viitehallinta/src/main/java/ohtaajat/bibtex/viitehallinta/validators/BookValidator.java
@@ -15,13 +15,13 @@ public class BookValidator implements Validator{
 
     @Override
     public void validate(Object o, Errors errors) {
-        BookFormObject book = (BookFormObject) o;
-        onKaikkiPakollinen(book, errors);
+//        BookFormObject book = (BookFormObject) o;
+//        onKaikkiPakollinen(book, errors);
     }
     
     public void onKaikkiPakollinen(BookFormObject book, Errors errors) {
-        if(book.getTitle().isEmpty()){
-            errors.rejectValue("title", "virheellinen_arvo", "virheellinen arvo");
-        }
+//        if(book.getTitle().isEmpty()){
+//            errors.rejectValue("title", "virheellinen_arvo", "virheellinen arvo");
+//        }
     }
 }
