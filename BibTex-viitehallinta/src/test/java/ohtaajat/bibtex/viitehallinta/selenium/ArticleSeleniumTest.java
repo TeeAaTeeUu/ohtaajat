ee61f99cb1577b9acfde22bb164124597a14c512
Teemu Siltanen <teemu.siltanen@helsinki.fi>
2013-04-25 21:54:01 +0300

    testeja

---
+++ BibTex-viitehallinta/src/test/java/ohtaajat/bibtex/viitehallinta/selenium/ArticleSeleniumTest.java
@@ -0,0 +1,47 @@
+
+package ohtaajat.bibtex.viitehallinta.selenium;
+
+import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Teemu
+ */
+public class ArticleSeleniumTest {
+    
+    private WebDriver driver;
+ 
+   
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
+    
+    @Test
+    public void addingNewArticleWorks() throws Exception {
+        driver.get("http://localhost:8080/");
+
+        WebElement element = driver.findElement(By.linkText("Create new article"));
+        element.click();
+        WebElement author = driver.findElement(By.name("author"));
+        WebElement title = driver.findElement(By.name("title"));
+        WebElement year = driver.findElement(By.name("year"));
+        WebElement journal = driver.findElement(By.name("journal"));
+       
+        author.sendKeys("arti3");
+        title.sendKeys("arti otsikko");
+        year.sendKeys("2005");
+        journal.sendKeys("joku");
+        author.submit();
+        
+        assertEquals(true, driver.getPageSource().contains("arti3"));
+        assertEquals(true, driver.getPageSource().contains("arti otsikko"));
+    }
+  
+
+}
---
+++ BibTex-viitehallinta/src/test/java/ohtaajat/bibtex/viitehallinta/selenium/InproceedingSeleniumTest.java
@@ -0,0 +1,47 @@
+
+package ohtaajat.bibtex.viitehallinta.selenium;
+
+import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Teemu
+ */
+public class InproceedingSeleniumTest {
+    
+    private WebDriver driver;
+ 
+   
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
+    
+    @Test
+    public void addingNewInproceedingWorks() throws Exception {
+        driver.get("http://localhost:8080/");
+
+        WebElement element = driver.findElement(By.linkText("Create new inproceeding"));
+        element.click();
+        WebElement author = driver.findElement(By.name("author"));
+        WebElement title = driver.findElement(By.name("title"));
+        WebElement year = driver.findElement(By.name("year"));
+        WebElement booktitle = driver.findElement(By.name("booktitle"));
+       
+        author.sendKeys("inpro3");
+        title.sendKeys("inpro otsikko");
+        year.sendKeys("2005");
+        booktitle.sendKeys("joku");
+        author.submit();
+        System.out.println(driver.getPageSource());
+        assertEquals(true, driver.getPageSource().contains("inpro3"));
+        assertEquals(true, driver.getPageSource().contains("inpro otsikko"));
+    }
+  
+
+}
